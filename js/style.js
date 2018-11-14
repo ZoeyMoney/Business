e23095ad64cf508201bf433e2c7d015222320e69
VeryGoodYou <1984898516@qq.com>
2018-11-14 19:03:36 +0800

    busi

---
+++ js/style.js
@@ -0,0 +1,21 @@
+
+
+$(window).on('load',function () {
+/*
+var $home = $('#yu'),
+    $hom = $home.find('#imm');
+$hom.fadeIn();
+$home.delay(950).fadeOut(1000);
+*/
+$('body').fadeIn(1500);
+
+    /*录播图*/
+    var mySwiper = new Swiper('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+});
+
